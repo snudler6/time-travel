652ddedee7b8cf8baf1192c4442cd03f3bdba0a0
snudler6 <snudler6@gmail.com>
2017-07-07 20:40:02 +0300

    Make the project work with tox

--- src/time_travel/datetime_patcher.py
+++ src/time_travel/datetime_patcher.py
@@ -1,23 +1,27 @@
-""""""
+"""A patch to the datetime module."""
 
 import datetime as datetime_lib
-from functools import partial
 import mock
 
 
-def lib_name():
-    return 'datetime' 
-
-
 class datetime_patcher(object):
+    """Context-manager patching the datetime module.
+    
+    For now patching only the @today function.
+    """
     
     def __init__(self, start_time=0):
+        """Create the patch.
+        
+        @start_time is time in seconds since the epoch.
+        """
         self.timestamp = start_time
         
         self.real_datetime = datetime_lib.datetime.fromtimestamp
         
         self.datetime = mock.Mock(wraps=datetime_lib.datetime)
-        self.datetime.mock_add_spec(["today", "now",  "utcnow", "fromtimestamp"])
+        self.datetime.mock_add_spec(
+            ["today", "now", "utcnow", "fromtimestamp"])
         
         today_mock = mock.Mock(side_effect=self.get_time)
         self.datetime.today = today_mock
@@ -25,9 +29,14 @@ class datetime_patcher(object):
         self.datetime_patcher = mock.patch('datetime.datetime', self.datetime)
 
     def set_time(self, timestamp):
+        """Set the time returned by now functions.
+        
+        @timestamp - is time in seconds since the epoch.
+        """
         self.timestamp = timestamp
         
     def get_time(self):
+        """Return a datetime object of the currently set time."""
         return self.real_datetime(self.timestamp)
     
     def __enter__(self):
@@ -37,5 +46,3 @@ class datetime_patcher(object):
         
     def __exit__(self, type, value, traceback):
         self.datetime_patcher.stop()
-
-        
