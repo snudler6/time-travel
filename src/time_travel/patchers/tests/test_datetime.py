8ffa83d2c992b008ccaeba5450cfc2ff83427c3f
snudler6 <snudler6@gmail.com>
2017-07-20 20:49:59 +0300

    more of not saving the "real" interface

--- src/time_travel/patchers/tests/test_datetime.py
+++ src/time_travel/patchers/tests/test_datetime.py
@@ -4,18 +4,15 @@ from time_travel.time_travel import TimeMachineClock
 import datetime
 
 
-real_fromtimestamp = datetime.datetime.fromtimestamp
-
-
 def test_datetime_patch():
     clock = TimeMachineClock()
     patcher = DatetimePatcher(clock)
     patcher.start()
     
-    assert datetime.datetime.today() == real_fromtimestamp(0)
+    assert datetime.datetime.today() == datetime.datetime.fromtimestamp(0)
     
     clock.timestamp = 3600
-    assert datetime.datetime.today() == real_fromtimestamp(3600)
+    assert datetime.datetime.today() == datetime.datetime.fromtimestamp(3600)
     patcher.stop()
     
     
@@ -24,13 +21,8 @@ def test_patcher_stop():
     patcher = DatetimePatcher(clock)
     patcher.start()
     
-    assert datetime.datetime.today() == real_fromtimestamp(0)
+    assert datetime.datetime.today() == datetime.datetime.fromtimestamp(0)
     
     patcher.stop()
     
-    assert datetime.datetime.today() != real_fromtimestamp(0)
-
-
-def test_patch_not_covering_classmethods():
-    assert datetime.datetime.fromtimestamp(0) == real_fromtimestamp(0)
-    assert datetime.datetime.fromtimestamp(3600) == real_fromtimestamp(3600)
+    assert datetime.datetime.today() != datetime.datetime.fromtimestamp(0)
