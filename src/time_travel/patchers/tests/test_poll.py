de679f7526aec1a3ac981d19d3d369e12477bc18
Shachar Ohana <shachar.o@gmail.com>
2017-09-08 00:15:43 +0300

    #20 - Some things I forgot

--- src/time_travel/patchers/tests/test_poll.py
+++ src/time_travel/patchers/tests/test_poll.py
@@ -35,150 +35,139 @@ class TestPollPatcher(object):
         with pytest.raises(ValueError):
             self.poll.poll()
 
-    def test_one_event(self):
-        event = mock.MagicMock()
-        self.events_pool.add_future_event(2,
-                                          event,
-                                          select.POLLIN)
+    def test_one_fd(self):
+        fd = mock.MagicMock()
+        self.events_pool.add_future_event(2, fd, select.POLLIN)
 
-        self.poll.register(event, select.POLLIN)
+        self.poll.register(fd, select.POLLIN)
 
-        assert self.poll.poll(sec2msec(10)) == [(event, select.POLLIN)]
+        assert self.poll.poll(sec2msec(10)) == [(fd, select.POLLIN)]
         assert self.clock.time == 2
 
     def test_timeout_before_event(self):
-        event = mock.MagicMock()
-        self.events_pool.add_future_event(10,
-                                          event,
-                                          select.POLLIN)
+        fd = mock.MagicMock()
+        self.events_pool.add_future_event(10, fd, select.POLLIN)
 
-        self.poll.register(event, select.POLLIN)
+        self.poll.register(fd, select.POLLIN)
 
         assert self.poll.poll(sec2msec(5)) == []
         assert self.clock.time == 5
         
     def test_unregistered_events(self):
-        first_event = mock.MagicMock(name='first_event')
-        second_event = mock.MagicMock(name='second_event')
-        expected_event = mock.MagicMock(name='expected_event')
+        first_fd = mock.MagicMock(name='first_fd')
+        second_fd = mock.MagicMock(name='second_fd')
+        expected_fd = mock.MagicMock(name='expected_fd')
         
-        self.events_pool.add_future_event(3,
-                                          first_event,
-                                          select.POLLIN)
-        self.events_pool.add_future_event(4,
-                                          second_event,
-                                          select.POLLIN)
-        self.events_pool.add_future_event(5,
-                                          expected_event,
-                                          select.POLLIN)
+        self.events_pool.add_future_event(3, first_fd, select.POLLIN)
+        self.events_pool.add_future_event(4, second_fd, select.POLLIN)
+        self.events_pool.add_future_event(5, expected_fd, select.POLLIN)
 
-        self.poll.register(expected_event, select.POLLIN)
+        self.poll.register(expected_fd, select.POLLIN)
         
-        assert self.poll.poll() == [(expected_event, select.POLLIN)]
+        assert self.poll.poll() == [(expected_fd, select.POLLIN)]
         assert self.clock.time == 5
         
-    def test_multiple_objects_same_time(self):
-        event1 = mock.MagicMock(name='event1')
-        event2 = mock.MagicMock(name='event2')
-        event3 = mock.MagicMock(name='event3')
-        far_event = mock.MagicMock(name='far_event')
-        unwaited_event = mock.MagicMock(name='unwaited_event')
+    def test_multiple_fds_same_time(self):
+        fd1 = mock.MagicMock(name='fd1')
+        fd2 = mock.MagicMock(name='fd2')
+        fd3 = mock.MagicMock(name='fd3')
+        far_fd = mock.MagicMock(name='far_fd')
+        unwaited_fd = mock.MagicMock(name='unwaited_fd')
         
-        self.events_pool.add_future_event(3, event1, select.POLLIN)
-        self.events_pool.add_future_event(3, event2, select.POLLIN)
-        self.events_pool.add_future_event(3, event3, select.POLLIN)
-        self.events_pool.add_future_event(5, far_event, select.POLLIN)
-        self.events_pool.add_future_event(2, unwaited_event, select.POLLIN)
+        self.events_pool.add_future_event(3, fd1, select.POLLIN)
+        self.events_pool.add_future_event(3, fd2, select.POLLIN)
+        self.events_pool.add_future_event(3, fd3, select.POLLIN)
+        self.events_pool.add_future_event(5, far_fd, select.POLLIN)
+        self.events_pool.add_future_event(2, unwaited_fd, select.POLLIN)
 
-        self.poll.register(event1, select.POLLIN)
-        self.poll.register(event2, select.POLLIN)
-        self.poll.register(event3, select.POLLIN)
-        self.poll.register(far_event, select.POLLIN)
+        self.poll.register(fd1, select.POLLIN)
+        self.poll.register(fd2, select.POLLIN)
+        self.poll.register(fd3, select.POLLIN)
+        self.poll.register(far_fd, select.POLLIN)
 
-        assert set(self.poll.poll()) == set([(event1, select.POLLIN),
-                                             (event2, select.POLLIN),
-                                             (event3, select.POLLIN)])
+        assert set(self.poll.poll()) == set([(fd1, select.POLLIN),
+                                             (fd2, select.POLLIN),
+                                             (fd3, select.POLLIN)])
         assert self.clock.time == 3
 
-    def test_same_object_multiple_events(self):
-        event = mock.MagicMock()
+    def test_same_fd_multiple_events(self):
+        fd = mock.MagicMock()
 
-        self.events_pool.add_future_event(3, event, select.POLLIN)
-        self.events_pool.add_future_event(3, event, select.POLLOUT)
-        self.events_pool.add_future_event(5, event, select.POLLHUP)
+        self.events_pool.add_future_event(3, fd, select.POLLIN)
+        self.events_pool.add_future_event(3, fd, select.POLLOUT)
+        self.events_pool.add_future_event(5, fd, select.POLLHUP)
 
-        self.poll.register(event,
-                           select.POLLIN | select.POLLOUT | select.POLLHUP)
+        self.poll.register(fd, select.POLLIN | select.POLLOUT | select.POLLHUP)
 
-        assert self.poll.poll() == [(event, select.POLLIN | select.POLLOUT)]
+        assert self.poll.poll() == [(fd, select.POLLIN | select.POLLOUT)]
         assert self.clock.time == 3
 
     def test_event_not_in_mask(self):
-        event = mock.MagicMock()
+        fd = mock.MagicMock()
 
-        self.events_pool.add_future_event(3, event, select.POLLIN)
+        self.events_pool.add_future_event(3, fd, select.POLLIN)
 
-        self.poll.register(event, select.POLLOUT)
+        self.poll.register(fd, select.POLLOUT)
 
         assert self.poll.poll(sec2msec(25)) == []
         assert self.clock.time == 25
 
     def test_event_not_returned_twice(self):
-        event = mock.MagicMock()
+        fd = mock.MagicMock()
         
-        self.events_pool.add_future_event(3, event, select.POLLIN)
-        self.poll.register(event, select.POLLIN)
+        self.events_pool.add_future_event(3, fd, select.POLLIN)
+        self.poll.register(fd, select.POLLIN)
 
-        assert self.poll.poll(sec2msec(5)) == [(event, select.POLLIN)]
+        assert self.poll.poll(sec2msec(5)) == [(fd, select.POLLIN)]
         assert self.clock.time == 3
 
         assert self.poll.poll(sec2msec(5)) == []
         assert self.clock.time == 3 + 5
           
     def test_same_event_multiple_timestamps(self):
-        event = mock.MagicMock()
+        fd = mock.MagicMock()
         
-        self.events_pool.add_future_event(1, event, select.POLLIN)
-        self.events_pool.add_future_event(2, event, select.POLLIN)
-        self.events_pool.add_future_event(2, event, select.POLLOUT)
+        self.events_pool.add_future_event(1, fd, select.POLLIN)
+        self.events_pool.add_future_event(2, fd, select.POLLIN)
+        self.events_pool.add_future_event(2, fd, select.POLLOUT)
 
-        self.poll.register(event, select.POLLIN | select.POLLOUT)
+        self.poll.register(fd, select.POLLIN | select.POLLOUT)
 
-        assert self.poll.poll() == [(event, select.POLLIN)]
+        assert self.poll.poll() == [(fd, select.POLLIN)]
         assert self.clock.time == 1
 
-        assert self.poll.poll() == [(event, select.POLLIN | select.POLLOUT)]
+        assert self.poll.poll() == [(fd, select.POLLIN | select.POLLOUT)]
         assert self.clock.time == 2
 
-    def test_unregister_event(self):
-        event = mock.MagicMock()
+    def test_unregister(self):
+        fd = mock.MagicMock()
 
-        self.events_pool.add_future_event(1, event, select.POLLIN)
-        self.events_pool.add_future_event(5, event, select.POLLIN)
+        self.events_pool.add_future_event(1, fd, select.POLLIN)
+        self.events_pool.add_future_event(5, fd, select.POLLIN)
 
-        self.poll.register(event, select.POLLIN)
+        self.poll.register(fd, select.POLLIN)
 
-        assert self.poll.poll() == [(event, select.POLLIN)]
+        assert self.poll.poll() == [(fd, select.POLLIN)]
         assert self.clock.time == 1
 
-        self.poll.unregister(event)
+        self.poll.unregister(fd)
 
         assert self.poll.poll(sec2msec(10)) == []
         assert self.clock.time == 1 + 10
 
-    def test_modify_event(self):
-        event = mock.MagicMock()
+    def test_modify(self):
+        fd = mock.MagicMock()
 
-        self.events_pool.add_future_event(1, event, select.POLLIN)
-        self.events_pool.add_future_event(5, event, select.POLLIN)
-        self.events_pool.add_future_event(10, event, select.POLLOUT)
+        self.events_pool.add_future_event(1, fd, select.POLLIN)
+        self.events_pool.add_future_event(5, fd, select.POLLIN)
+        self.events_pool.add_future_event(10, fd, select.POLLOUT)
 
-        self.poll.register(event, select.POLLIN)
+        self.poll.register(fd, select.POLLIN)
 
-        assert self.poll.poll() == [(event, select.POLLIN)]
+        assert self.poll.poll() == [(fd, select.POLLIN)]
         assert self.clock.time == 1
 
-        self.poll.modify(event, select.POLLOUT)
+        self.poll.modify(fd, select.POLLOUT)
 
-        assert self.poll.poll() == [(event, select.POLLOUT)]
+        assert self.poll.poll() == [(fd, select.POLLOUT)]
         assert self.clock.time == 10
