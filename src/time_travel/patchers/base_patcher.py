94bacee483fa47847cf59451288b978c3a50e2eb
snudler6 <nudler6@hotmail.com>
2017-09-30 18:56:59 +0300

    moved the credit documetnation

--- src/time_travel/patchers/base_patcher.py
+++ src/time_travel/patchers/base_patcher.py
@@ -51,7 +51,33 @@ class BasePatcher(object):
         raise NotImplementedError()
         
     def start(self):
-        """Start the patcher."""
+        """Start the patcher.
+        
+        The logic to the patchers start is based on the work done by:
+        spulec/freezegun
+        under
+        https://github.com/spulec/freezegun
+        
+        Copyright (C) 2017 spulec/freezegun
+        
+        Licensed under the Apache License, Version 2.0 (the "License"); you may
+        not use this file except in compliance with the License. You may obtain
+        a copy of the License at
+        
+          http://www.apache.org/licenses/LICENSE-2.0
+        
+        Unless required by applicable law or agreed to in writing,
+        software distributed under the License is distributed on an
+        "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+        KIND, either express or implied.  See the License for the
+        specific language governing permissions and limitations
+        under the License.
+        
+        Modifications:
+        
+        Modifications to the file was to leave the inner change of the loaded
+        modules and removing any other related logic to a specific module.
+        """
         patch_actions = self.get_patch_actions()
         
         real_id_to_fake = {id(real): fake for _, real, fake in
