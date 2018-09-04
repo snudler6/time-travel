9b37721461578a791ea5f741efb2655309ccd350
Shachar Nudler <nudler6@hotmail.com>
2018-09-04 21:43:43 +0300

    Bump version: 1.1.0 → 1.1.1 (#59)

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ author = 'Shachar Nudler'
 # built documents.
 #
 # The short X.Y version.
-version = '1.1.0'
+version = '1.1.1'
 # The full version, including alpha/beta/rc tags.
 release = version
 
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 import select
 
 
-version = '1.1.0'
+version = '1.1.1'
 
 
 patchers = [
